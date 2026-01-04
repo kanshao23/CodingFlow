53f75938cfd2d55642dd939c60143753521dffeb
kan shao <kanshaous@gmail.com>
2026-01-04 08:26:00 +0800

    Initial Commit

---
+++ CodingFlow/CodingFlowApp.swift
@@ -0,0 +1,32 @@
+//
+//  CodingFlowApp.swift
+//  CodingFlow
+//
+//  Created by Kan Shao on 2026/1/4.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CodingFlowApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ CodingFlowTests/CodingFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodingFlowTests.swift
+//  CodingFlowTests
+//
+//  Created by Kan Shao on 2026/1/4.
+//
+
+import Testing
+@testable import CodingFlow
+
+struct CodingFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
